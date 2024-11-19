2eaa1696caadc462ca2709d0136e97f9acf3e372
Nguyen Manh Tuan <mguyenmanhtuan@gmail.com>
2024-11-19 11:24:28 +0700

    Update BottomBar.kt

--- app/src/main/java/com/capstone/bookshelf/feature/readbook/presentation/component/bottomBar/BottomBar.kt
+++ app/src/main/java/com/capstone/bookshelf/feature/readbook/presentation/component/bottomBar/BottomBar.kt
@@ -428,3 +428,139 @@ fun BottomBarDefault(
         Spacer(modifier = Modifier.height(10.dp))
     }
 }
+
+@Composable
+fun BottomBarAutoScroll(
+    bookContentViewModel: BookContentViewModel,
+    ttsState : TTSState,
+    uiState : ContentUIState,
+    onPreviousChapterIconClick: () -> Unit,
+    onPlayPauseIconClick: () -> Unit,
+    onNextChapterIconClick: () -> Unit,
+    onMusicIconClick: () -> Unit,
+    onStopIconClick: () -> Unit,
+    onSettingIconClick: () -> Unit,
+){
+    val iconList = listOf(
+        R.drawable.ic_previous_chapter,
+        R.drawable.ic_play,
+        R.drawable.ic_pause,
+        R.drawable.ic_next_chapter,
+        //R.drawable.ic_music
+        R.drawable.ic_stop,
+        //R.drawable.ic_setting
+    )
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .background(MaterialTheme.colorScheme.surfaceContainer),
+        horizontalAlignment = Alignment.CenterHorizontally,
+
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth()
+                            .wrapContentHeight()
+                            .padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ){
+            IconButton(
+                modifier = Modifier.size(50.dp),
+                onClick = {
+                    onPreviousChapterIconClick()
+                }
+            ) {
+                Icon(
+                    modifier = Modifier.size(30.dp),
+                    painter = painterResource(id = iconList[0]),
+                    contentDescription = "previous chapter"
+                )
+            }
+            IconButton(
+                modifier = Modifier.size(50.dp),
+                onClick = {
+                    onPlayPauseIconClick()
+                }
+            ) {
+                if(ttsState.isSpeaking) {
+                    Icon(
+                        modifier = Modifier.size(30.dp),
+                        painter = painterResource(id = iconList[1]),
+                        contentDescription = "play/pause"
+                    )
+                }else{
+                    Icon(
+                        modifier = Modifier.size(30.dp),
+                        painter = painterResource(id = iconList[2]),
+                        contentDescription = "play/pause"
+                    )
+                }
+            }
+            IconButton(
+                modifier = Modifier.size(50.dp),
+                onClick = {
+                    onNextChapterIconClick()
+                }
+            ) {
+                Icon(
+                    modifier = Modifier.size(30.dp),
+                    painter = painterResource(id = iconList[3]),
+                    contentDescription = "next chapter"
+                )
+            }
+        }
+        Row(
+            modifier = Modifier.fillMaxWidth()
+                            .wrapContentHeight()
+                            .padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ){
+            IconButton(
+                modifier = Modifier.size(50.dp),
+                onClick = {
+                    onMusicIconClick()
+                }
+            ) {
+                Icon(
+                    modifier = Modifier.size(30.dp),
+                    painter = painterResource(id = iconList[4]),
+                    contentDescription = "music"
+                )
+            }
+            IconButton(
+                modifier = Modifier.size(50.dp),
+                onClick = {
+                    onStopIconClick()
+                }
+            ) {
+                Icon(
+                    modifier = Modifier.size(30.dp),
+                    painter = painterResource(id = iconList[4]),
+                    contentDescription = "stop"
+                )
+            }
+            IconButton(
+                modifier = Modifier.size(50.dp),
+                onClick = {
+                    onSettingIconClick()
+                }
+            ) {
+                Icon(
+                    modifier = Modifier.size(30.dp),
+                    painter = painterResource(id = iconList[4]),
+                    contentDescription = "setting"
+                )
+            }
+        }
+        if(uiState.openTTSMusicMenu){
+            MusicMenuDialog(
+                bookContentViewModel = bookContentViewModel
+            )
+        }
+        // if(uiState.openAutoScrollMenu){
+        //     AutoScrollMenuDialog(
+        //         bookContentViewModel = bookContentViewModel
+        //     )
+        // }
+    }
+}
