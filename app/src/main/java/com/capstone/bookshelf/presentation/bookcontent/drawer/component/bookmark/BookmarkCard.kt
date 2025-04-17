335dd76575858b1a5fe2200e1911aa5edb9ff321
kuro-mujou <mguyenmanhtuan@gmail.com>
2025-04-17 11:35:32 +0700

    fix slow loading bookmark menu, add animation for card background

--- app/src/main/java/com/capstone/bookshelf/presentation/bookcontent/drawer/component/bookmark/BookmarkCard.kt
+++ app/src/main/java/com/capstone/bookshelf/presentation/bookcontent/drawer/component/bookmark/BookmarkCard.kt
@@ -1,9 +1,14 @@
 package com.capstone.bookshelf.presentation.bookcontent.drawer.component.bookmark
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -19,6 +24,9 @@ import androidx.compose.material3.TooltipBox
 import androidx.compose.material3.TooltipDefaults
 import androidx.compose.material3.rememberTooltipState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -34,6 +42,7 @@ import com.capstone.bookshelf.presentation.component.BookmarkShape
 import com.capstone.bookshelf.util.darken
 import com.capstone.bookshelf.util.isDark
 import com.capstone.bookshelf.util.lighten
+import kotlinx.coroutines.delay
 
 @OptIn(ExperimentalMaterial3Api::class)
 @UnstableApi
@@ -55,6 +64,12 @@ fun BookmarkCard(
     val tooltipState = rememberTooltipState(
         isPersistent = true
     )
+    val isBackgroundReady = remember { mutableStateOf(false) }
+
+    LaunchedEffect(bookmarkIndex) {
+        delay(500)
+        isBackgroundReady.value = true
+    }
     val cardComposable : @Composable () -> Unit = {
         ElevatedCard(
             shape = BookmarkShape(),
@@ -81,54 +96,64 @@ fun BookmarkCard(
                 modifier = Modifier.fillMaxSize(),
                 contentAlignment = Alignment.Center
             ) {
-                when (bookmarkStyle) {
-                    BookmarkStyle.WAVE_WITH_BIRDS -> {
-                        CardBackgroundWaveWithBirds(
-                            modifier = Modifier.matchParentSize(),
-                            baseColor = baseColor
-                        )
-                    }
+                Column(
+                    modifier = Modifier.matchParentSize()
+                ) {
+                    AnimatedVisibility(
+                        visible = isBackgroundReady.value,
+                        enter = fadeIn(animationSpec = tween(durationMillis = 1000)),
+                        exit = fadeOut(animationSpec = tween(durationMillis = 1000))
+                    ) {
+                        when (bookmarkStyle) {
+                            BookmarkStyle.WAVE_WITH_BIRDS -> {
+                                CardBackgroundWaveWithBirds(
+                                    modifier = Modifier.fillMaxSize(),
+                                    baseColor = baseColor
+                                )
+                            }
 
-                    BookmarkStyle.CLOUD_WITH_BIRDS -> {
-                        CardBackgroundCloudWithBirds(
-                            modifier = Modifier.matchParentSize(),
-                            baseColor = baseColor
-                        )
-                    }
+                            BookmarkStyle.CLOUD_WITH_BIRDS -> {
+                                CardBackgroundCloudWithBirds(
+                                    modifier = Modifier.fillMaxSize(),
+                                    baseColor = baseColor
+                                )
+                            }
 
-                    BookmarkStyle.STARRY_NIGHT -> {
-                        CardBackgroundStarryNight(
-                            modifier = Modifier.matchParentSize(),
-                            baseColor = baseColor
-                        )
-                    }
+                            BookmarkStyle.STARRY_NIGHT -> {
+                                CardBackgroundStarryNight(
+                                    modifier = Modifier.fillMaxSize(),
+                                    baseColor = baseColor
+                                )
+                            }
 
-                    BookmarkStyle.GEOMETRIC_TRIANGLE -> {
-                        CardBackgroundGeometricTriangle(
-                            modifier = Modifier.matchParentSize(),
-                            baseColor = baseColor
-                        )
-                    }
+                            BookmarkStyle.GEOMETRIC_TRIANGLE -> {
+                                CardBackgroundGeometricTriangle(
+                                    modifier = Modifier.fillMaxSize(),
+                                    baseColor = baseColor
+                                )
+                            }
 
-                    BookmarkStyle.POLYGONAL_HEXAGON -> {
-                        CardBackgroundPolygonalHexagon(
-                            modifier = Modifier.matchParentSize(),
-                            baseColor = baseColor
-                        )
-                    }
+                            BookmarkStyle.POLYGONAL_HEXAGON -> {
+                                CardBackgroundPolygonalHexagon(
+                                    modifier = Modifier.fillMaxSize(),
+                                    baseColor = baseColor
+                                )
+                            }
 
-                    BookmarkStyle.SCATTERED_HEXAGON -> {
-                        CardBackgroundScatteredHexagons(
-                            modifier = Modifier.matchParentSize(),
-                            baseColor = baseColor
-                        )
-                    }
+                            BookmarkStyle.SCATTERED_HEXAGON -> {
+                                CardBackgroundScatteredHexagons(
+                                    modifier = Modifier.fillMaxSize(),
+                                    baseColor = baseColor
+                                )
+                            }
 
-                    BookmarkStyle.CHERRY_BLOSSOM_RAIN -> {
-                        CardBackgroundCherryBlossomRain(
-                            modifier = Modifier.matchParentSize(),
-                            baseColor = baseColor
-                        )
+                            BookmarkStyle.CHERRY_BLOSSOM_RAIN -> {
+                                CardBackgroundCherryBlossomRain(
+                                    modifier = Modifier.fillMaxSize(),
+                                    baseColor = baseColor
+                                )
+                            }
+                        }
                     }
                 }
                 Box(
