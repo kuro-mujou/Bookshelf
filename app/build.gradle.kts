aa0db01ac2a337c066942ca7e656008a353574eb
kuro-mujou <mguyenmanhtuan@gmail.com>
2025-05-23 16:13:46 +0700

    bump version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.capstone.bookshelf"
         minSdk = 26
         targetSdk = 35
-        versionCode = 12
-        versionName = "2.0.6"
+        versionCode = 13
+        versionName = "2.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
